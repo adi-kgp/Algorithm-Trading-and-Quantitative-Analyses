a44ddd7d88a14cd050c3e2e605d7a33f29887554
adi-kgp <adi.kgp@gmail.com>
2022-09-16 19:05:23 +0530

    added script in 'basic data handling' section

--- Basic Data Handling/basic_visualizations.py
+++ Basic Data Handling/basic_visualizations.py
@@ -10,12 +10,9 @@ import pandas as pd
 import yfinance as yf
 import matplotlib.pyplot as plt
 
-stocks = ['AMZN', 'MSFT', 'META', 'GOOG']
-
+tickers = ['AMZN', 'MSFT', 'META', 'GOOG']
 start = dt.datetime.today() - dt.timedelta(4350)
-
 end = dt.datetime.today() - dt.timedelta(700)
-
 cl_price = pd.DataFrame()
 
 for ticker in tickers:
@@ -32,3 +29,14 @@ daily_return.plot(subplots=True, layout=(2,2), title="Daily Returns Evolution")
 # In quantitative finance, we visualize compounded returns
 
 (1 + daily_return).cumprod().plot()
+
+## Using Core matplotlib.pyplot module
+
+fig, ax = plt.subplots()
+plt.style.available
+plt.style.use("ggplot")
+ax.set(title="Mean Daily Return of Stocks", xlabel="Stocks", ylabel="Mean Return")
+plt.bar(x=daily_return.columns, height=daily_return.mean())
+plt.bar(x=daily_return.columns, height=daily_return.std())
+
+
